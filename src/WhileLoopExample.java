c90693b2103939083eb0e8ca43502a2f3acd9b97
Siljal <silja.lehiste@gmail.com>
2021-10-16 11:23:07 +0300

    My first commit.

---
+++ src/WhileLoopExample.java
@@ -0,0 +1,13 @@
+public class WhileLoopExample {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        do {
+            System.out.print(i++);
+            System.out.print("x");
+        } while (i < 5);
+
+
+    }
+}
